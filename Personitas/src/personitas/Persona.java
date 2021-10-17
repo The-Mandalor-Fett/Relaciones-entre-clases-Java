f3dd768633bcccad10733e4ae6853c5bae696eae
The-Mandalore-Fett <davidc.cortes@outlook.com>
2021-10-17 12:51:58 -0500

    Creamos el constructor y un método de la clase padre

--- Personitas/src/personitas/Persona.java
+++ Personitas/src/personitas/Persona.java
@@ -8,9 +8,26 @@ package personitas;
  *
  * @author david
  */
-public abstract class Persona
+public abstract class Persona//abstracto, no sabemos realmente que es, osea
+//no sabemos quien es la persona
 {
   //Una persona tiene su nombre, edad, sexo, y posiblemente
   //un Correo
-  
+  protected String nombre;
+  protected int edad;
+  protected String sexo;
+  protected String correo;
+  public Persona()//constructor
+  {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.sexo = sexo;
+    this.correo = correo;
+  }
+  public void showData()//método mostrar datos(no abstracto)
+  {
+    System.out.println("Datos de la personita");
+    
+  }
+
 }
