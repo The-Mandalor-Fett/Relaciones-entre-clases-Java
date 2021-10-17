a1ef6d2b44f1d0e608ff4a6f2b274a3fedb56afb
The-Mandalore-Fett <davidc.cortes@outlook.com>
2021-10-17 12:45:02 -0500

    Creamos la clase padre abstracta llamada persona

---
+++ Personitas/src/personitas/Persona.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personitas;
+
+/**
+ *
+ * @author david
+ */
+public abstract class Persona
+{
+  //Una persona tiene su nombre, edad, sexo, y posiblemente
+  //un Correo
+  
+}
