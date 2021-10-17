cada665da1f1e0cb85b648f267b9520c411123db
The-Mandalore-Fett <davidc.cortes@outlook.com>
2021-10-17 13:49:17 -0500

    Creamos el constructor de la subclase alumno

--- Personitas/src/personitas/Alumno.java
+++ Personitas/src/personitas/Alumno.java
@@ -8,7 +8,21 @@ package personitas;
  *
  * @author david
  */
-public class Alumno 
+ //Esta clase es hida de Persona y debe implementar
+ //Aprendiz según lo mostrado en la imagen
+public class Alumno extends Persona implements Aprendiz
 {
-    
+  //De la clase padre hereda algunas cosas como nombre, etc.
+  //Pero esta calse va a tener sus propios atributos
+  private int matricula;
+  private String carrera;
+  private String grupo;
+  public Alumno(String nombre,int edad,String sexo,String correo, int matricula,String carrera,String grupo)
+  {//constructor
+    super(nombre,edad,sexo,correo);//De la super clase tenemos
+    this.matricula = matricula;
+    this.carrera = carrera;
+    this.grupo = grupo;
+  }
+  //Tenemosque implementar los métodos abstractos de la clase padre
 }
