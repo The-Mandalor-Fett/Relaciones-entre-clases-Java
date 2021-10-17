55a674d44644e005d607ddd09c30221ac8a0d4d2
The-Mandalore-Fett <davidc.cortes@outlook.com>
2021-10-17 15:20:36 -0500

    Implementamos lo métodos de la clase que hereda

--- Personitas/src/personitas/Docente.java
+++ Personitas/src/personitas/Docente.java
@@ -8,7 +8,31 @@ package personitas;
  *
  * @author david
  */
-public class Docente 
+public class Docente extends Persona implements Contribuyente
 {
-    
+  //colocamos sus propias variables
+  private String area
+  private String cargo
+  private int ident
+  public Docente(String nombre,int edad, String sexo, String correo,String area,String cargo, int ident)
+  {
+    super(nombre,edad,sexo);
+    this.area = area;
+    this.cargo = cargo;
+    this.ident = ident;
+  }
+  @Override
+  public void showData()
+  {
+    System.out.println("La personita es un docente");
+    super.showData();//imprime los datos de la super clase
+    System.out.println("Area: "+ this.Area);//Datos de la clase alumno
+    System.out.println("ID: "+ this.IDen);
+  }
+  @Override
+  protected void hablar()
+  {
+    System.out.println("El profesor dice: Jovenes contesten que es para hoy");  
+  }
+
 }
