361dd26c1a577a090045dcaf4e2173957641ae74
The-Mandalore-Fett <davidc.cortes@outlook.com>
2021-10-17 19:23:58 -0500

    Agregamos constructor y métodos heredados

--- Personitas/src/personitas/Autoridad.java
+++ Personitas/src/personitas/Autoridad.java
@@ -8,7 +8,30 @@ package personitas;
  *
  * @author david
  */
-public class Autoridad 
+public class Autoridad extends Persona implements Contribuyente
 {
-    
+  private String area;
+  private String cargo;
+  private int ident;
+  public Autoridad(String nombre,int edad, String sexo, String correo,String area,String cargo, int ident)
+  {
+    super(nombre,edad,sexo,correo);
+    this.area = area;
+    this.cargo = cargo;
+    this.ident = ident;
+  }
+  @Override
+  public void showData()
+  {
+    System.out.println("La personita es una autoridad");
+    super.showData();//imprime los datos de la super clase
+    System.out.println("Area: "+ this.area);//Datos de la clase alumno
+    System.out.println("Cargo: "+ this.cargo);
+    System.out.println("ID: "+ this.ident);
+  }
+  @Override
+  protected void hablar()
+  {
+    System.out.println("La autoridad dice: Miren los nuevos lineamiento para regresar");
+  }
 }
